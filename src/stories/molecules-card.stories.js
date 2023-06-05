23a628babe11ba3188c15a5a7e36d09fdacdf37c
yurie-22 <r.toukyo.22.12.743@4nonome.com>
2023-06-05 16:27:21 +0900

    storybookを開くためのコミット

---
+++ src/stories/molecules-card.stories.js
@@ -0,0 +1,4 @@
+import Item_Card from "../components/molecules/card/card.html";
+import "../components/molecules/card/card.scss";
+export default { title: "molecules/card" };
+export const card = () => Item_Card;
